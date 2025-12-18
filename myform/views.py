1fbe7c1108a936c8d134a2d398663cf4fbb6c16b
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-18 20:25:18 +0600

    views file

--- myform/views.py
+++ myform/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.timezone import now
@@ -8,28 +8,31 @@ def signup_view(request):
         first_name = request.POST.get("first_name")
         email = request.POST.get("email")
 
+        # Email context
         context = {
             "site_name": "Raihan World",
-            "user": {
-                "first_name": first_name
-            },
+            "user": {"first_name": first_name},
             "current_year": now().year,
         }
 
+        # Render email template
         html_content = render_to_string("massage.html", context)
 
+        # Send email
         email_message = EmailMultiAlternatives(
             subject="Welcome to Raihan World!",
             body="Welcome to Raihan World!",
             from_email="Raihan World <noreply@raihanworld.com>",
             to=[email],
         )
-
         email_message.attach_alternative(html_content, "text/html")
         email_message.send()
 
-        return render(request, "form.html", {
-            "message": "Welcome email sent successfully!"
-        })
+        # Redirect to GET page with query param
+        return redirect('/?success=1')
 
-    return render(request, "form.html")
+    # GET request
+    success = request.GET.get('success')
+    return render(request, "form.html", {
+        "message": "Welcome email sent successfully!" if success else None
+    })
