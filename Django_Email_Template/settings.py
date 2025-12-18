a251f5106c9f69246ff86a419aee4f9b79df163c
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-18 20:48:18 +0600

    modify

--- Django_Email_Template/settings.py
+++ Django_Email_Template/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%5tjj(y*o=!^5tlp6j3ivbwhm^s+#i*ht&@#hnq2s!2gqduk)*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['raihan-emailsend.vercel.app', 'localhost']
 
 
 # Application definition
